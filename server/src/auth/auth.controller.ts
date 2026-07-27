fa930fdf321a96e6337d8cc810265bc4780189ad
Alexander <SaSkrkr123.com@mail.ru>
2026-07-27 15:39:14 +0300

    implement user registration

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,11 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Регистрация пользователя',
+  })
   @Post('register')
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
--- server/src/auth/dto/register.dto.ts
+++ server/src/auth/dto/register.dto.ts
@@ -1,14 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
 import { Role } from '@prisma/client';
 
 export class RegisterDto {
+  @ApiProperty({
+    example: 'user@mail.com',
+  })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    example: '123456',
+  })
   @IsString()
   @MinLength(6)
   password!: string;
 
+  @ApiProperty({
+    enum: Role,
+    example: Role.BUYER,
+  })
   @IsEnum(Role)
   role!: Role;
 }
